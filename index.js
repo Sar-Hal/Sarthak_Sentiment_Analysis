006f363410434eeeb79e6a409713a005b9b01694
nishantjazz <nishantjazzedu@gmail.com>
2025-01-18 06:05:10 +0530

    Update for route handling

--- index.js
+++ index.js
@@ -3,47 +3,49 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
-const cookiePaser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 const Blog = require("./models/blog");
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 
-const {
-  checkForAuthenticationCookie,
-} = require("./middlewares/authentication");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then((e) => console.log("MongoDB Connected"));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(cookiePaser());
+app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
 
-
 app.get("/", async (req, res) => {
-  const allBlogs = await Blog.find({});
-  res.render("home", {
-    user: req.user,
-    blogs: allBlogs,
-  });
+  try {
+    const allBlogs = await Blog.find({});
+    res.render("home", {
+      user: req.user,
+      blogs: allBlogs,
+    });
+  } catch (err) {
+    console.error("Error fetching blogs:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
-
-app.get("/test", (req, res) => {
-  res.render("test");
-});
-
 
 app.use("/user", userRoute);
 app.use("/blog", blogRoute);
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+app.all("*", (req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+module.exports = app;
